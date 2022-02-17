e60485a8b1723a653d64d17344e2c41062fe636f
Maria Daan <mariadaan@gmail.com>
2022-02-17 17:14:27 +0100

    alter struct and new functions

--- micro_paint.c
+++ micro_paint.c
@@ -15,18 +15,12 @@ typedef struct s_info
 	int		bg_height;
 	char	bg_char;
 
-	float	x_empty; // the horizontal position of the top left corner of the rectangle
-	float	y_empty; // the vertical position of the top left corner of the rectangle
-	float	width_empty; // >0
-	float	height_empty; // >0
-	char	r_borderrect;
-
-	float	x_filled; // the horizontal position of the top left corner of the rectangle
-	float	Y_filled; // the vertical position of the top left corner of the rectangle
-	float	width_filled; // >0
-	float	height_filled; // >0
-	char	r_filledrect;
-
+	char	rect_type;
+	float	x; // the horizontal position of the top left corner of the rectangle
+	float	y; // the vertical position of the top left corner of the rectangle
+	float	width; // >0
+	float	height; // >0
+	char	rect_char;
 
 } t_info;
 
@@ -47,17 +41,74 @@ void print_file(FILE *fp)
 	}
 }
 
-int parse_input(FILE *fp, t_info *info)
-{
-	char specifier;
+// int parse_input(FILE *fp, t_info *info)
+// {
+// 	char specifier;
+// 	int ret;
 
-	fscanf(fp, "%d %d %c", &info->bg_width, &info->bg_height, &info->bg_char);
-	fscanf(fp, "\n%c ", &specifier);
-	if (specifier == 'r')
+// 	while (fscanf(fp, "%f %f %f %f %c", &info->x, &info->y, &info->width, &info->height, &info->rect_type))
+// 	{
+		
+// 		if (info->rect_type == 'r')
+// 			draw_empty(info);
+// 		else if (info->rect_type == 'R')
+// 			draw_full(info);
+// 		else
+// 		{
+// 			write(1, "Error: Operation file corrupted\n", 32);
+// 			return (0);
+// 		}
+// 	}
+// 	return (0);
+// }
+
+void draw_empty(t_info *info, char **matrix)
+{
+	int x_int;
+	int y_int;
+	int width_int;
+	int height_int;
+
+	x_int = ceil(info->x);
+	y_int = ceil(info->y);
+	width_int = ceil(info->width);
+	height_int = ceil(info->height);
+
+	printnum("x_int", x_int);
+	printnum("y_int", y_int);
+	printnum("width_int", width_int);
+	printnum("height_int", height_int);
+	printf("\n");
+
+	// horizontal rows
+	for (int i_x = info->x; i_x <= width_int; i_x++)
 	{
-		fscanf(fp, "%f %f %f %f %c", &info->x_empty, &info->y_empty, &info->width_empty, &info->height_empty, &info->r_borderrect);
+		matrix[y_int][i_x] = info->rect_char;
+		matrix[y_int + height_int][i_x] = info->rect_char;
 	}
-	
+
+	// vertical columns
+	for (int i_y = y_int; i_y <= height_int; i_y++)
+	{
+		matrix[i_y][x_int] = info->rect_char;
+		matrix[i_y][x_int + width_int] = info->rect_char;
+	}
+}
+
+int alter_matrix(FILE *fp, t_info *info, char **matrix)
+{
+	while (fscanf(fp, "%c %f %f %f %f %c\n", &info->rect_type, &info->x, &info->y, &info->width, &info->height, &info->rect_char) != EOF)
+	{
+		if (info->rect_type == 'r')
+			draw_empty(info, matrix);
+		// else if (info->rect_type == 'R')
+		// 	draw_full(info, matrix);
+		else
+		{
+			write(1, "Error: Operation file corrupted\n", 32);
+			return (0);
+		}
+	}
 	return (0);
 }
 
@@ -100,11 +151,21 @@ char **create_matrix(int width, int height, char c)
 	return matrix;
 }
 
+int get_background(FILE *fp, t_info *info)
+{
+	int ret;
+	ret = fscanf(fp, "%d %d %c\n", &info->bg_width, &info->bg_height, &info->bg_char);
+	printnum("bg", info->bg_width);
+	printnum("return", ret);
+	// check if values are valid
+	return (0);
+}
+
 int main(int argc, char *argv[])
 {
 	t_info info;
 	FILE * fp;
-	char **painting;
+	char **matrix;
 
 	if (argc != 2)
 	{
@@ -113,10 +174,12 @@ int main(int argc, char *argv[])
 	}
 	char *filename = argv[1];
 	fp = fopen(filename, "r");
+	get_background(fp, &info);
+	matrix = create_matrix(info.bg_width, info.bg_height, info.bg_char);
+	alter_matrix(fp, &info, matrix);
 	// print_file(fp);
-	parse_input(fp, &info);
-	painting = create_matrix(info.bg_width, info.bg_height, info.bg_char);
-	print_2D(painting, info.bg_height);
+	// parse_input(fp, &info);
+	print_2D(matrix, info.bg_height);
 	printfloat("info.x_empty", info.x_empty);
 	printfloat("info.width_empty", info.width_empty);
 
